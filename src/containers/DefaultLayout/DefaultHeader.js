c23ff70bd0afd9cbb390584c32c7eab04dce1dae
ChienTruong <qchien2400@gmail.com>
2019-06-09 15:11:51 +0700

    Clean up

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
+import { AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 import PropTypes from 'prop-types';
-
-import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/logo.svg'
-import sygnet from '../../assets/img/brand/sygnet.svg'
+import React, { Component } from 'react';
+import { DropdownToggle, Nav } from 'reactstrap';
+import logo from '../../assets/img/brand/logo.svg';
+import sygnet from '../../assets/img/brand/sygnet.svg';
 
 const propTypes = {
   children: PropTypes.node,
