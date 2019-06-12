41157669523a8d19df5fc2a210dc26186bd37080
ChienTruong <chientq@itvsoft.asia>
2019-06-12 11:52:03 +0700

    Test UI, add rules eslint, and edit readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Documents
-
 [React Bootstrap](https://react-bootstrap.github.io/)
+### Template ADMIN
+[Preview](https://coreui.io/demo/#main.html)
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,40 +1,71 @@
 import React, { Component } from 'react';
-import { Pagination, PaginationItem, PaginationLink, Table } from 'reactstrap';
+import { Table } from 'reactstrap';
 
+const headers = ['Name', 'Image'];
+
+const fields = ['name', 'imageUrl'];
 export default class Product extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      products: []
-    }
+      products: [],
+      displayedProducts: [],
+    };
   }
 
   componentDidMount() {
-      
+    // Simulator
+    const products = [
+      { id: 1, name: 'Chien', imageUrl: 'chienUrl' },
+      { id: 2, name: 'Truong', imageUrl: 'truongUrl' },
+    ];
+    this.setState({ displayedProducts: products });
   }
 
   render() {
+
+    const displayedDatas = this.state.displayedProducts;
+    const contentHeader = headers.map(ele => <th key={ele}>{ele}</th>);
+    const contentBody = displayedDatas.map(ele => {
+      const contents = fields.map(field => {
+        return (
+          <td key={ele[field]}>{ele[field]}</td>
+        );
+      });
+
+      const buttons = <td key={ele.id}>
+        <a className="btn btn-success" href="#">
+          <i className="fa fa-edit"></i>
+        </a>
+        <a className="btn btn-danger" href="#">
+          <i className="fa fa-trash-o"></i>
+        </a>
+      </td>;
+
+      contents.push(buttons);
+
+      return (
+        <tr key={ele.id}>
+          {contents}
+        </tr>
+      );
+    });
     return (
       <div>
         <Table responsive striped>
           <thead>
             <tr>
+              {contentHeader}
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
+            {contentBody}
           </tbody>
         </Table>
-        <Pagination>
-          <PaginationItem disabled><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
-          <PaginationItem active><PaginationLink tag="button">1</PaginationLink></PaginationItem>
-          <PaginationItem><PaginationLink tag="button">2</PaginationLink></PaginationItem>
-          <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
-          <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
-          <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
-        </Pagination>
       </div>
-    )
+    );
   }
 }
